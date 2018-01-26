ead5086689fbd3860853fc92862d00f9f0fae1ee
Henry Ohanga <ohanga.henry@gmail.com>
2018-01-27 02:31:43 +0300

    reset homepage to github user

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -121,8 +121,8 @@ module.exports = {
           // "url" loader works just like "file" loader but it also embeds
           // assets smaller than specified size as data URLs to avoid requests.
           {
-            // test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-            // loader: require.resolve('url-loader'),
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
               name: 'static/media/[name].[hash:8].[ext]',
