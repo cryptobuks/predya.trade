18d07789bb44b3caafe52bd066538b17e0bc39b5
Henry Ohanga <ohanga.henry@gmail.com>
2018-03-09 21:24:46 +0300

    Adds predya configs

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = {
-	NAME: 'Bitnoww',
-	DOMAIN: 'http://bitnoww.particles.co.ke',
+	NAME: 'Predya',
+	DOMAIN: 'http://predya.trade',
 	API_BASE_URL: 'https://api.nexchange.io/en/api/v1',
-	SUPPORT_EMAIL: 'support@bitnoww.com',
+	SUPPORT_EMAIL: 'support@predya.trade.com',
 	PRICE_FETCH_INTERVAL: 60000,
 	ORDER_DETAILS_FETCH_INTERVAL: 20000,
 	RECENT_ORDERS_INTERVAL: 20000,
 	RECENT_ORDERS_COUNT: 11,
 	PRICE_COMPARISON_INTERVAL: 60000,
-	REFERRAL_CODE: 'RVDJWDGATLA'
+	REFERRAL_CODE: 'RQY7FXMKXWK'
 };
 
 export default config;
