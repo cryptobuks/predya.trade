6de31c859630aac741d5b692f6a6ec491fe7dc5c
Henry Ohanga <ohanga.henry@gmail.com>
2018-03-14 22:31:31 +0300

    Changes the withdrawal address to BTC address

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = {
-	NAME: 'Predya',
+	NAME: 'Predya Trade',
 	DOMAIN: 'http://predya.trade',
 	API_BASE_URL: 'https://api.nexchange.io/en/api/v1',
-	SUPPORT_EMAIL: 'support@predya.trade.com',
+	SUPPORT_EMAIL: 'support@predya.trade',
 	PRICE_FETCH_INTERVAL: 60000,
 	ORDER_DETAILS_FETCH_INTERVAL: 20000,
 	RECENT_ORDERS_INTERVAL: 20000,
 	RECENT_ORDERS_COUNT: 11,
 	PRICE_COMPARISON_INTERVAL: 60000,
-	REFERRAL_CODE: 'RQY7FXMKXWK'
+	REFERRAL_CODE: 'R7SB8W5EDKQ' //'RQY7FXMKXWK'
 };
 
 export default config;
